b7a8dfce9b9e523045250bfb9f8b2bacbfbca6c1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-12 17:38:33 +0100

    init

---
+++ src/main/kotlin/com/durys/jakub/happeningservice/HappeningServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.durys.jakub.happeningservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HappeningServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<HappeningServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/durys/jakub/happeningservice/HappeningServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.happeningservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HappeningServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
