300119c4955256e07432af86f3574b505c2f3de5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-12 20:58:50 +0100

    participant

--- src/main/kotlin/com/durys/jakub/happeningservice/happening/domain/Happening.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/happening/domain/Happening.kt
@@ -1,4 +1,9 @@
 package com.durys.jakub.happeningservice.happening.domain
 
-class Happening(val id: HappeningId, val number: Number, val place: Place, val period: Period) {
+class Happening(val id: HappeningId, val number: Number, val place: Place,
+                val period: Period, val participants: MutableList<Participant> = mutableListOf()) {
+
+
+
+    fun invite(participants: List<Participant>) = this.participants.addAll(participants)
 }
--- src/main/kotlin/com/durys/jakub/happeningservice/happening/domain/Participant.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/happening/domain/Participant.kt
@@ -1,4 +1,9 @@
 package com.durys.jakub.happeningservice.happening.domain
 
-class Participant {
+import java.util.UUID
+
+class Participant(val id: ParticipantId, val name: String) {
 }
+
+
+class ParticipantId(val value: UUID)
