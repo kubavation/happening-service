82d79eea292fc81439ecd1bbe8d2b2925191db7c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-13 20:24:26 +0100

    invitation factory + tests

--- src/main/kotlin/com/durys/jakub/happeningservice/invitation/domain/Invitation.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/invitation/domain/Invitation.kt
@@ -2,10 +2,19 @@ package com.durys.jakub.happeningservice.invitation.domain
 
 import com.durys.jakub.happeningservice.happening.domain.HappeningNumber
 import com.durys.jakub.happeningservice.sharedkernel.ParticipantId
+import java.time.LocalDate
 
-internal class Invitation(val id: InvitationId) {
+internal class Invitation(private val id: InvitationId, private val validTill: LocalDate) {
 
     fun id() = id
+
+
+    companion object Factory {
+        fun create(participantId: ParticipantId, happeningNumber: HappeningNumber, validTill: LocalDate): Invitation {
+            return Invitation(InvitationId(participantId, happeningNumber), validTill)
+        }
+    }
+
 }
 
 internal data class InvitationId(val participantId: ParticipantId, val happeningNumber: HappeningNumber)
