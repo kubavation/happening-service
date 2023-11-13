8dfacb763c860b1ac589ae36f7b9cf59cd676217
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-13 20:14:55 +0100

    initial HappeningOpenedEventHandler

---
+++ src/main/kotlin/com/durys/jakub/happeningservice/invitation/application/event/handler/HappeningOpenedEventHandler.kt
@@ -0,0 +1,19 @@
+package com.durys.jakub.happeningservice.invitation.application.event.handler
+
+import com.durys.jakub.happeningservice.happening.domain.event.HappeningOpened
+import com.durys.jakub.happeningservice.invitation.infrastructure.InMemoryInvitationRepository
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+internal class HappeningOpenedEventHandler(private val invitationRepository: InMemoryInvitationRepository) {
+
+
+    @EventListener
+    fun on(event: HappeningOpened) {
+
+        //todo handle
+
+    }
+
+}
