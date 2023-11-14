976ab782ad22a041ad38ba50c3edecd523a15eff
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-14 16:43:47 +0100

    invitation reply vo

--- src/main/kotlin/com/durys/jakub/happeningservice/invitation/domain/Invitation.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/invitation/domain/Invitation.kt
@@ -4,14 +4,16 @@ import com.durys.jakub.happeningservice.happening.domain.HappeningNumber
 import com.durys.jakub.happeningservice.sharedkernel.ParticipantId
 import java.time.LocalDate
 
-internal class Invitation(private val id: InvitationId, private val validTill: LocalDate) {
+internal class Invitation(private val id: InvitationId, private val validTill: LocalDate,
+                          private var reply: InvitationReply) {
 
     fun id() = id
+    fun validTill() = validTill
 
 
     companion object Factory {
         fun create(participantId: ParticipantId, happeningNumber: HappeningNumber, validTill: LocalDate): Invitation {
-            return Invitation(InvitationId(participantId, happeningNumber), validTill)
+            return Invitation(InvitationId(participantId, happeningNumber), validTill, InvitationReply(false))
         }
     }
 
