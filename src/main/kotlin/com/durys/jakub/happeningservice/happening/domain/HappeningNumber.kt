bc8355201dec88fdd86371c6aa134bb986af9ecd
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-14 17:20:37 +0100

    build fix

--- src/main/kotlin/com/durys/jakub/happeningservice/happening/domain/HappeningNumber.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/happening/domain/HappeningNumber.kt
@@ -2,5 +2,7 @@ package com.durys.jakub.happeningservice.happening.domain
 
 internal data class HappeningNumber(private val value: String) {
 
+    fun value() = value
+
     constructor(period: Period, place: Place): this("${place.name} [${period.from.toLocalDate()}-${period.to.toLocalDate()}]")
 }
