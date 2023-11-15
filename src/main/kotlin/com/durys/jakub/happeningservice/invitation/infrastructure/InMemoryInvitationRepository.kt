c1e2f8713b8b52eeed02b6f5f8ec66bea9630d4f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-15 21:55:50 +0100

    additional method in invitation repo -> loading all invitations by happening number

--- src/main/kotlin/com/durys/jakub/happeningservice/invitation/infrastructure/InMemoryInvitationRepository.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/invitation/infrastructure/InMemoryInvitationRepository.kt
@@ -1,5 +1,6 @@
 package com.durys.jakub.happeningservice.invitation.infrastructure
 
+import com.durys.jakub.happeningservice.happening.domain.HappeningNumber
 import com.durys.jakub.happeningservice.invitation.domain.Invitation
 import com.durys.jakub.happeningservice.invitation.domain.InvitationId
 import com.durys.jakub.happeningservice.invitation.domain.InvitationNumber
@@ -15,6 +16,10 @@ internal class InMemoryInvitationRepository: InvitationRepository {
 
     override fun load(number: InvitationNumber): Invitation {
         return db.values.find { it.number() == number.value } ?: throw RuntimeException("Happening not found")
+    }
+
+    override fun loadAll(happeningNumber: HappeningNumber): List<Invitation> {
+        return db.filter { entry -> entry.key.happeningNumber === happeningNumber }.values.toList()
     }
 
     override fun save(invitation: Invitation): InvitationId {
