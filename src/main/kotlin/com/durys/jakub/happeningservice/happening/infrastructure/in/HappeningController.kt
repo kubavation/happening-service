836d2f627ad9a14d6e9177f89aa1adba0dbcabd5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-18 11:27:04 +0100

    HappeningController

--- src/main/kotlin/com/durys/jakub/happeningservice/happening/infrastructure/in/HappeningController.kt
+++ src/main/kotlin/com/durys/jakub/happeningservice/happening/infrastructure/in/HappeningController.kt
@@ -1,5 +1,9 @@
 package com.durys.jakub.happeningservice.happening.infrastructure.`in`
 
+import com.durys.jakub.happeningservice.happening.application.HappeningApplicationService
+import com.durys.jakub.happeningservice.happening.domain.HappeningId
+import com.durys.jakub.happeningservice.happening.domain.command.AppendInvitationPatternCommand
+import com.durys.jakub.happeningservice.sharedkernel.invitation.content.InvitationQuestion
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -9,10 +13,19 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("happenings")
-internal class HappeningController {
+internal class HappeningController(val happeningApplicationService: HappeningApplicationService) {
 
     @PostMapping("/{happeningId}/invitation-content")
     fun appendInvitationContent(@PathVariable happeningId: UUID, @RequestBody content: InvitationContentDTO) {
-        //todo
+        happeningApplicationService.handle(
+                AppendInvitationPatternCommand(HappeningId(happeningId), content.title, content.description, asQuestions(content.questions))
+        )
+    }
+
+
+    private fun asQuestions(questions: Set<InvitationQuestionDTO>): Set<InvitationQuestion> {
+        return questions
+                .map { InvitationQuestion(it.question, it.required, it.type, it.availableOptions) }
+                .toSet()
     }
 }
